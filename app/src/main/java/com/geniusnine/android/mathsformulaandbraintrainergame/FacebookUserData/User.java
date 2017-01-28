6d96801ddb39f6ecf88528e2477367a265555248
geniusninedev <developer@nineinfosys.com>
2017-01-28 15:25:50 +0530

    Facebook Login Details

---
+++ app/src/main/java/com/geniusnine/android/mathsformulaandbraintrainergame/FacebookUserData/User.java
@@ -0,0 +1,19 @@
+package com.geniusnine.android.mathsformulaandbraintrainergame.FacebookUserData;
+
+/**
+ * Created by Dev on 28-01-2017.
+ */
+
+public class User {
+
+
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+
+
+}
