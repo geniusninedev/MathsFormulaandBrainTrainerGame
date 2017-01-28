aea4138326ea2a009c84847a070356a15388e45c
geniusninedev <developer@nineinfosys.com>
2017-01-28 19:02:17 +0530

    Created Webviews and html by sravani

--- app/src/main/java/com/geniusnine/android/mathsformulaandbraintrainergame/formula/geometry/CallGeometryDetails.java
+++ app/src/main/java/com/geniusnine/android/mathsformulaandbraintrainergame/formula/geometry/CallGeometryDetails.java
@@ -44,10 +44,46 @@ public class CallGeometryDetails extends AppCompatActivity {
         } else if (position == 5) {
             geometryDetailsWebView.loadUrl("file:///android_res/raw/lozenge.html");
         } else if (position == 6) {
-            geometryDetailsWebView.loadUrl("file:///android_res/raw/baba.html");
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/trepezoid.html");
+        }else if (position == 7) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/quadri.html");
+        } else if (position == 8) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/circle.html");
+        } else if (position == 9) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/segment.html");
+        } else if (position == 10) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/sector.html");
+
+        } else if (position == 11) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/regularpolygon.html");
+        } else if (position == 12) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/hexagon.html");
+        } else if (position == 13) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/sphere.html");
+        } else if (position == 14) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/sphericalcap.html");
+        } else if (position == 15) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/sphericalsphere.html");
+        } else if (position == 16) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/sphericalsector.html");
+        }
+        else if (position == 17) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/torus.html");
+        } else if (position == 18) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/cylinder.html");
+        } else if (position == 19) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/cone.html");
+        } else if (position == 20) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/frustum.html");
+
+        } else if (position == 21) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/pyramid.html");
+        } else if (position == 22) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/cuboid.html");
+        }else if (position == 23) {
+            geometryDetailsWebView.loadUrl("file:///android_res/raw/triprism.html");
         }
     }
-
     class WebViewClient extends android.webkit.WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
